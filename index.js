f8afd2e4faadd8c4f7adf852f6f0bec30dfab165
francescobianco <info.francescobianco@gmail.com>
2020-05-14 02:02:15 +0200

    upload

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var http = require('http');
 
-const port = process.env.PORT || 8080;
+const jq = require('node-jq');
+const port = process.env.PORT || 3000;
 
 //create a server object:
 http.createServer((req, res) => {
-    res.write('Hello World!'); //write a response to the client
-    res.end(); //end the response
+    jq.run('.foo', '{ "foo": "bar" }', { input: 'string' }).then((value)=> {
+        res.write(value); //write a response to the client
+        res.end(); //end the response
+    }).catch((error) => {
+        res.write(error.message); //write a response to the client
+        res.end(); //end the response
+    })
 }).listen(port); //the server object listens on port 8080
